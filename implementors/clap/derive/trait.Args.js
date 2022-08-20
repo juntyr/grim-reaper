9f97803f23dbe1ce0dbf68cff3ef312897d701bc
MomoLangenstein <MomoLangenstein@users.noreply.github.com>
2022-08-20 09:19:11 +0000

    deploy: 66114ace1fccb0e817c1b98a3b7400b2abba7199

---
+++ implementors/clap/derive/trait.Args.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"grim_reaper":[["impl Args for <a class=\"struct\" href=\"grim_reaper/struct.GrimReaper.html\" title=\"struct grim_reaper::GrimReaper\">GrimReaper</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/clap/derive/trait.Parser.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"cargo_reaper":[["impl Parser for <a class=\"enum\" href=\"cargo_reaper/enum.Cmd.html\" title=\"enum cargo_reaper::Cmd\">Cmd</a>"]],
+"grim_reaper":[["impl Parser for <a class=\"struct\" href=\"grim_reaper/struct.GrimReaper.html\" title=\"struct grim_reaper::GrimReaper\">GrimReaper</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
