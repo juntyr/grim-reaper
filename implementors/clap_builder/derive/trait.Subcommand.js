2a859286a755eb1a8a29d480d4e395b9bba2f6d4
juntyr <juntyr@users.noreply.github.com>
2023-03-28 17:13:12 +0000

    deploy: 81ad78d59c33baff611afbef37ec4a229b668d54

---
+++ implementors/clap_builder/derive/trait.Subcommand.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"cargo_reaper":[["impl Subcommand for <a class=\"enum\" href=\"cargo_reaper/enum.Cmd.html\" title=\"enum cargo_reaper::Cmd\">Cmd</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
